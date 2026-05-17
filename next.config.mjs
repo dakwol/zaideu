2eaebb6d923a6b7ed49c395ca30e9dfe1b49dcf1
dakwol <daniilfomenko99@gmail.com>
2026-05-18 00:41:45 +0400

    upd git info

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
+const repositoryName = 'zaideu';
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+
+  basePath: `/${repositoryName}`,
+  assetPrefix: `/${repositoryName}/`,
+
+  trailingSlash: true,
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
